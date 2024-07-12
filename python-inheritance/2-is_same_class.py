8ef7fcab045ebc12a4a60d224c1a1c9d84c219f2
missnoel <a.odero@alustudent.com>
2024-07-12 07:26:38 -0700

    Exact same object

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+'''new function is_same_class '''
+
+
+def is_same_class(obj, a_class):
+    """Returns True if obj is exactly an instance of a_class; otherwise False"""
+    return type(obj) is a_class
