3739afe9afc92103f056b8901c88af5034049d1d
missnoel <a.odero@alustudent.com>
2024-07-12 08:29:27 -0700

    Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+''' creating a class '''
+
+
+class BaseGeometry:
+    # instance method
+
+    def area(self):
+        '''raises an exception'''
+        raise Exception('area() is not implemented')
