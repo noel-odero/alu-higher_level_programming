08412fb6150cb8b2e68d4342fe7a7a7d4e5c6592
missnoel <a.odero@alustudent.com>
2024-07-16 03:50:16 -0700

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
+'''function definition '''
+
+
 def read_file(filename=""):
+    '''function to read file '''
+
     with open(filename, 'r', encoding='utf-8') as fp:
+        # opening a file with read access mode
+
         print(fp.read())
 
