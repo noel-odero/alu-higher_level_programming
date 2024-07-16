d57f74018c8f10eb44dcc7c4ba3f81a30ce5375e
missnoel <a.odero@alustudent.com>
2024-07-16 08:20:41 -0700

    Load, add, save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+'''Add all args to a python list and save them to a file '''
+
+import sys
+import os.path
+
+
+if __name__=="__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+    filename = "add_item.json"
+
+    # Check if the file exists, and load the data if it does
+    if os.path.exists(filename):
+        items = load_from_json_file(filename)
+    else:
+        items = []
+
+     # Add all arguments to the list
+    items.extend(sys.argv[1:])
+
+    # Save the updated list back to the file
+    save_to_json_file(items, filename)
