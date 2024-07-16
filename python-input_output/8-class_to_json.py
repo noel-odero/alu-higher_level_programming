a48fd9618a41ff8ffcb0c4cc8a92ec446a024384
missnoel <a.odero@alustudent.com>
2024-07-16 08:45:39 -0700

    Class to JSON

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-'''module that returns a dictionary description with a simple 
+'''module that returns a dictionary description with a simple
 data structure'''
 
 
 def class_to_json(obj):
     """
-    Returns the dictionary description with simple data structure 
+    Returns the dictionary description with simple data structure
     """
     return obj.__dict__
