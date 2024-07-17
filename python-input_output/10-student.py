3738f25d2e3d2f2780a51a1508776ff386f45176
missnoel <a.odero@alustudent.com>
2024-07-17 01:54:46 -0700

    Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module that defines the class Student
+"""
+
+
+class Student:
+    """ Class to create student instances """
+
+    def __init__(self, first_name, last_name, age):
+        """ Special method to initialize """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Method that returns directory description """
+        obj = self.__dict__.copy()
+        
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
