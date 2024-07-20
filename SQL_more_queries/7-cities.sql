e7c615feb0b7995e4ccf6854c42e1bec37711610
missnoel <a.odero@alustudent.com>
2024-07-20 12:45:40 -0700

    cities table

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+--  a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa)
+
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS `cities`.`hbtn_0d_usa` (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL,
+        name VARCHAR(256) NOT NULL,
+        FOREIGN KEY (state_id) REFERENCES states(id)
+);
