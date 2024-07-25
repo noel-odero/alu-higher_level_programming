9d83f076c2345dd3ef9b8ca67caf848acb90e406
noel-odero <anoel.odero@gmail.com>
2024-07-25 16:30:21 +0300

    Catch me if you can!

--- python-network_0/102-catch_me.sh
+++ python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that makes a request to causes an specific response
-curl -sX PUT -L -d "user_id=98" -H "Origin:HolbertonSchool" "0.0.0.0:5000/catch_me"
+curl -sL 0.0.0.0:5000/catch_me
--- python-network_0/server.py
+++ python-network_0/server.py
@@ -1,13 +1,13 @@
+#!/usr/bin/python3
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/':
+        if self.path == '/catch_me':
             self.send_response(200)
-            self.send_header('Content-type', 'text/html')
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            response = "my index page is so cool! Mainly because it’s in Python"
-            self.wfile.write(response.encode('utf-8'))
+            self.wfile.write(b'You got me!')
 
 def run(server_class=HTTPServer, handler_class=MyHandler, port=5000):
     server_address = ('', port)
