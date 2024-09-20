c8b609ce600f8552ec84bdc4dde56473142ba2b6
missnoel <a.odero@alustudent.com>
2024-09-20 08:37:41 -0700

    addition

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-function add(a, b){
-	return(a+b);
+function add (a, b) {
+  return (a + b);
 }
+const argv = process.argv.slice(2);
+const y = parseInt(argv[0]);
+const z = parseInt(argv[1]);
 
-const argv = process.argv.slice(2)
-const y = parseInt(argv[0])
-const z = parseInt(argv[1])
-
-console.log(add(y, z))
+console.log(add(y, z));
