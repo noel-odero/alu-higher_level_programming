f5c219c085ab42ea69dc4af414c568977806c330
missnoel <a.odero@alustudent.com>
2024-09-20 05:32:13 -0700

    multiline using arraya and loop

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-let languages = ["C is fun", "Python is cool",  "JavaScript is amazing"]
-
-for (let i = 0; i < languages.length; i++){
-	console.log(languages[i]);
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+for (let i = 0; i < 3; i++) {
+  console.log(msg[i]);
 }
