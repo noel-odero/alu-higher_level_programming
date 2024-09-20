3ad6c60fde2b92bb3e3875162d8854dc22ebd0b8
missnoel <a.odero@alustudent.com>
2024-09-20 09:15:41 -0700

    second biggest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,2 +1,12 @@
 #!/usr/bin/node
 
+// Get arguments from the command line, excluding the first two elements (node and script path)
+const args = process.argv.slice(2).map(Number); // Convert all arguments to integers
+
+if (args.length <= 1) {
+  console.log(0); // Print 0 if no arguments or only one argument
+} else {
+  // Sort in descending order and select the second biggest
+  const secondBiggest = args.sort((a, b) => b - a)[1];
+  console.log(secondBiggest);
+}
