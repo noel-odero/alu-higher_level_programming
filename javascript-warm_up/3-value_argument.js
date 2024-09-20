b428249224b71f4cac829749c91242ffa648c373
Noel Odero <anoel.odero@gmail.com>
2024-09-20 14:26:41 +0300

    Value of my argument

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
-if (process.argv.slice(2)[0] === undefined) {
-    console.log('No argument');
-  } else {
-    console.log(process.argv.slice(2)[0]);
-  }
+const args = process.argv.slice(2);  // Get the arguments starting from the third one
+
+if (args[0] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(args[0]);
+}
