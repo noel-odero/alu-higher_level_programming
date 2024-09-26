fc949d111f8e929199addcb32643db1e83c4dd4c
missnoel <a.odero@alustudent.com>
2024-09-26 03:37:11 -0700

    square #0

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,31 +1,10 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
+const Rectangle = require('./4-rectangle');
 
-  print () {
-    const character = 'X';
-    for (let i = 0; i < this.height; i++) {
-      console.log(character.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    [this.width, this.height] = [this.height, this.width];
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
   }
 }
-class Square extends Rectangle{
-	constructor(size) {
-		super(size, size);
-	}
-}
+
 module.exports = Square;
