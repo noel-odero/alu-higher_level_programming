b8d51f8044d638ed6fd8705e40776c71bb27ae88
missnoel <a.odero@alustudent.com>
2024-09-26 03:32:35 -0700

    square #0

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    const character = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(character.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+class Square extends Rectangle{
+	constructor(size) {
+		super(size, size);
+	}
+}
+module.exports = Rectangle;
